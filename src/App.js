3905f8b6574cab40b4ef60a462b1075e51ef248f
Carlos Martinez <curlosmart@gmail.com>
2021-07-27 16:52:43 -0400

    API and HTML setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { getPhotosFromAPI } from './utils/api'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [searchText, setSearchText] = useState('')
+  const [photos, setPhotos] = useState([])
+
+  const handleChange = (event) => {
+    setSearchText(event.target.value)
+  }
+
+  const handleClick = async () => {
+    const photosFromAPI = await getPhotosFromAPI
+  }
+
   return (
     <div className="App">
       Image Search Gallery
+      <input type="text" className="locationInput" id="locationInput" onChange={handleChange}></input>
+      <button onClick={handleClick}>Get forecast</button>
     </div>
   );
 }
---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+const ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY
+
+export const getPhotosFromAPI = (searchText) => {
+    console.log(searchText)
+}
